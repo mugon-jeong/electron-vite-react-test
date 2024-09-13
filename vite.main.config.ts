24c193798467dc98bf3817175e3eaf6ffab14712
conny <conny@musma.net>
2024-09-13 11:40:52 +0900

    init

---
+++ vite.main.config.ts
@@ -0,0 +1,41 @@
+import path from "path";
+import type { ConfigEnv, UserConfig } from "vite";
+import { defineConfig, mergeConfig } from "vite";
+import {
+  getBuildConfig,
+  getBuildDefine,
+  external,
+  pluginHotRestart,
+} from "./vite.base.config";
+
+// https://vitejs.dev/config
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<"build">;
+  const { forgeConfigSelf } = forgeEnv;
+  const define = getBuildDefine(forgeEnv);
+  const config: UserConfig = {
+    build: {
+      lib: {
+        entry: forgeConfigSelf.entry!,
+        fileName: () => "[name].js",
+        formats: ["cjs"],
+      },
+      rollupOptions: {
+        external,
+      },
+    },
+    plugins: [pluginHotRestart("restart")],
+    define,
+    resolve: {
+      // Load the Node.js entry.
+      mainFields: ["module", "jsnext:main", "jsnext"],
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@main": path.resolve(__dirname, "./src/main"),
+        "@commands": path.resolve(__dirname, "./src/commands"),
+      },
+    },
+  };
+
+  return mergeConfig(getBuildConfig(forgeEnv), config);
+});
